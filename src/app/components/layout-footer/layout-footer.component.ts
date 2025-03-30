0571e0320e5d4857f7cc630bcf5f79b04d9612c0
Andre Parada <andreparada98@gmail.com>
2025-03-30 15:50:37 -0400

    feat: layout-footer

---
+++ src/app/components/layout-footer/layout-footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'LayoutFooter',
+    templateUrl: './layout-footer.component.html',
+    styleUrl: './layout-footer.component.scss',
+})
+export class LayoutFooterComponent {}
